e440d4944c18c6f11a97f4d6f668c5b23e084d34
Aniket Prakash <b218004@iiit-bh.ac.in>
2021-04-18 11:51:45 +0530

    ⚡ Integrated linkedin with probot.

--- post2linkedin/index.js
+++ post2linkedin/index.js
@@ -2,16 +2,28 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+ const child_process = require("child_process");
+ const util = require("util");
+ 
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  module.exports = (app) => {
-    app.on("pull_request", async (context) => {
-      // Code was pushed to the repo, what should we do with it?
-      app.log.info(context);
-    });
-  };
+  app.on("pull_request", async (context) => {
+
+    // Code was pushed to the repo, what should we do with it?
+    child_process.exec = util.promisify(child_process.exec);
+
+  const url = context.payload.pull_request.base.repo.html_url;
+  const url1= context.payload.pull_request.diff_url;
+  try {
+    const res =  child_process.exec(
+      `py ${__dirname}/server.py ${url} ${url1}`
+    );
+  } catch (er) {
+    console.log(er);
+  }
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
